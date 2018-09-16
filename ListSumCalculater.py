eae1167f3a49c6a33cd7aec9660dd7bde1b7edcf
suhaib kamal <suhaib49@gmail.com>
2018-09-16 23:15:55 +0300

    Initial commit

---
+++ ListSumCalculater.py
@@ -0,0 +1,30 @@
+def sum(x):
+    if x > 0:
+        return x + sum(x - 1)
+    else:
+        return 0
+
+
+inputList = []
+sumList = []
+
+
+def countSumList(x):
+    for i in x:
+        sumList.append(sum(i))
+
+
+def getListNumbers():
+    s = input("write finish to end...\n")
+    while s != 'finish':
+        try:
+            inputList.append(eval(s))
+            s = input()
+        except:
+            s = input("the value you entered can't be cast to int \n")
+
+
+getListNumbers()
+countSumList(inputList)
+
+print(sumList)
